86533706c1fbc6ee9b8191a51abbf87078bf1f4e
JeremMac <macal.jerem@gmail.com>
2023-09-12 14:28:20 +0200

    changing insert left instructions

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,8 +21,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
 		parent->left->parent = new_node;
+		new_node->left = parent->left;
 	}
 
 	parent->left = new_node;
