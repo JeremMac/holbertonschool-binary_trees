9e5f57f1c52c39eb8cf6999b0c00d8354749a920
JeremMac <macal.jerem@gmail.com>
2023-09-13 14:07:05 +0200

    Correcting tree height.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,49 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * check_left - A function that
- * checks the left side of a tree.
+ * binary_tree_height - A function that
+ * measures the height of a binary tree.
  * @tree: a pointer to the root
  * node of the tree to measure
  * the height.
  * Return: 0 if tree is NULL
  * else return height.
  */
-size_t check_left(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	height++;
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (0);
-	}
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		height++;
-	}
-
-	binary_tree_height(tree->left);
-	binary_tree_height(tree->right);
-
-	return (height);
-}
-
-/**
- * check_right - A function that
- * checks the right side of a tree.
- * @tree: a pointer to the root
- * node of the tree to measure
- * the height.
- * Return: 0 if tree is NULL
- * else return height.
- */
-size_t check_right(const binary_tree_t *tree)
-{
+	size_t height_left = 0;
+	size_t height_right = 0;
 	size_t height = 0;
 
 	if (tree == NULL)
@@ -60,33 +29,8 @@ size_t check_right(const binary_tree_t *tree)
 		height++;
 	}
 
-	binary_tree_height(tree->right);
-	binary_tree_height(tree->left);
-
-	return (height);
-}
-
-/**
- * binary_tree_height - A function that
- * measures the height of a binary tree.
- * @tree: a pointer to the root
- * node of the tree to measure
- * the height.
- * Return: 0 if tree is NULL
- * else return height.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t height_left = 0;
-	size_t height_right = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	height_left = check_left(tree);
-	height_right = check_right(tree);
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
 
 	if (height_left > height_right)
 	{
@@ -96,6 +40,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (height_right);
 	}
-	return (height_left);
+	return (height);
 }
 
