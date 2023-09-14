ede0ccf01ea3e24af1c8d68f0ae14e07536f49a0
Letho1972 <ericmarescq@gmail.com>
2023-09-14 15:57:51 +0200

    add modify function tree uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,10 +11,15 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *parent;
 	binary_tree_t *grandparent;
 
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+
 	parent = node->parent;
 	grandparent = parent->parent;
 
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
