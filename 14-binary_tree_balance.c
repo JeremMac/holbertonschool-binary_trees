b4e63a3369293db30c42a5b17abcb6b909aa02b6
JeremMac <macal.jerem@gmail.com>
2023-09-14 10:42:38 +0200

    Added tree balance funtion.

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - A function
+ * that measures the balance factor
+ * of a binary tree.
+ * @tree: A pointer to the root node
+ * of the tree to measure the balance
+ * factor.
+ * Return: an int value of the balance
+ * of the given tree.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	if (tree->left != NULL && tree->right == NULL)
+	{
+		return (left - right);
+	}
+	if (tree->left == NULL && tree->right != NULL)
+	{
+		return (right - left - 1);
+	}
+	left = binary_tree_balance(tree->left);
+	right = binary_tree_balance(tree->right);
+	return (left + right + 1);
+}
